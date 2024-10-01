b76648b29b74e5af99193e94e60b71b7083da059
PurvaP22 <165667443+PurvaP22@users.noreply.github.com>
2024-10-02 01:10:29 +0530

    Initial commit

---
+++ Stateless App/column_scenarios/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar:AppBar(
+          title: const Text("Column Demo",
+          style:TextStyle(fontSize: 30,fontWeight: FontWeight.w800),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+
+          body: Container(
+           width:MediaQuery.of(context).size.width,
+           color: Colors.pink,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end
+            ,
+            children: [
+              Container(
+                height:200,
+                width: 200,
+                color:Colors.black,
+              )
+            ],
+            ),
+          ),
+      ),
+      );
+    
+  }
+}
